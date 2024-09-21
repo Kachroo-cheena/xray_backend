534aa0f649e3efa962f0247dd56d5868e97808de
Cheena Kachroo <cheenakachroo2000@gmail.com>
2024-09-22 02:52:53 +0530

    versioning

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ np.set_printoptions(suppress=True)
 BASE_DIR = os.path.dirname(os.getcwd())
 MODEL_DIR = os.path.join(BASE_DIR, 'model')
 
-loaded_model = load_model(os.path.join(MODEL_DIR, 'keras_model.h5'), compile=False)
-class_names = open(os.path.join(MODEL_DIR, 'labels.txt'), "r").readlines()
+# loaded_model = load_model(os.path.join(MODEL_DIR, 'keras_model.h5'), compile=False)
+# class_names = open(os.path.join(MODEL_DIR, 'labels.txt'), "r").readlines()
 
-# loaded_model = load_model("c:/Users/lenovo/OneDrive/Desktop/insightx_v1/X-ray-Image-Classification/xray_tryout/x-ray-body-part-classification/model/keras_model.h5", compile=False)
-# class_names = open("c:/Users/lenovo/OneDrive/Desktop/insightx_v1/X-ray-Image-Classification/xray_tryout/x-ray-body-part-classification/model/labels.txt", 'r').readlines()
+loaded_model = load_model("model/keras_model.h5", compile=False)
+class_names = open("model/labels.txt", 'r').readlines()
 
 @app.route('/predict', methods=['POST'])
 def predict():
