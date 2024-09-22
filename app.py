c17cd0150e848de9627076e06971a27d37ec1504
Cheena Kachroo <cheenakachroo2000@gmail.com>
2024-09-22 13:24:35 +0530

    versioning

--- app.py
+++ app.py
@@ -50,6 +50,6 @@ def predict():
         'confidence_score': float(confidence_score)
     })
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port)
